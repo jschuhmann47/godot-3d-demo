61f13bcf377685ba2581f0d766f3b7802759afa9
Juani Schuhmann <juanischuhmann@gmail.com>
2025-09-28 17:23:23 -0300

    Add spawner

---
+++ mob/spawner/spawner_3d.gd
@@ -0,0 +1,14 @@
+extends Node3D
+
+## desc
+@export var mob_to_spawn: PackedScene = null
+
+@onready var marker_3d: Marker3D = %Marker3D
+@onready var timer: Timer = %Timer
+
+
+func _on_timer_timeout() -> void:
+	# When export, it preloads by itself
+	var new_mob = mob_to_spawn.instantiate()
+	add_child(new_mob)
+	new_mob.global_position = marker_3d.global_position
